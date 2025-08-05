e77e3ca88be12a72a0f7a27f10097d09729e2e1a
Dekoder-py <dekod3r.py@gmail.com>
2025-08-06 09:30:56 +1200

    reset cards 1 second after 2 are clicked that dont match,  clicked matching works

--- index.js
+++ index.js
@@ -1,20 +1,37 @@
 const cards = document.querySelectorAll('.card');
 
 var clicked = 0;
+var firstClick = null;
+var secondClick = null;
+
+function resetCards() {
+    setTimeout(() => {
+        clicked = 0;
+        cards.forEach(card => {
+            card.textContent = '?';
+        });
+    }, 1000);
+
+};
 
 cards.forEach(card => {
     card.addEventListener('click', () => {
         if (clicked < 2) {
             const value = card.getAttribute('data-card');
             console.log('Card clicked, value:', value)
-
             card.textContent = value;
             clicked += 1;
-        } else {
-            clicked = 0;
-            cards.forEach(card => {
-                card.textContent = '?';
-            });
+            if (clicked == 1)
+                firstClick = value;
+            else {
+                secondClick = value
+                if (firstClick == secondClick) {
+                    console.log("+1 score");
+                    clicked = 0;
+                } else
+                    resetCards();
+            };
         };
+
     });
 });
