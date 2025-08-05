9bf1151f84c46e36404b0b519829c6941283be81
Dekoder-py <dekod3r.py@gmail.com>
2025-08-05 21:50:28 +1200

    add card clicking logic - flip cards

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log('hello from index.js');
+const cards = document.querySelectorAll('.card');
+
+var clicked = 0;
+
+cards.forEach(card => {
+    card.addEventListener('click', () => {
+        if (clicked < 2) {
+            const value = card.getAttribute('data-card');
+            console.log('Card clicked, value:', value)
+
+            card.textContent = value;
+            clicked += 1;
+        } else {
+            clicked = 0;
+            cards.forEach(card => {
+                card.textContent = '?';
+            });
+        };
+    });
+});
