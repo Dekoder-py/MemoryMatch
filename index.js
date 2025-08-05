6382deb349fb9927e8b07a9cbfc0e9eb30587efe
Dekoder-py <dekod3r.py@gmail.com>
2025-08-06 09:40:07 +1200

    refactor card click logic to improve readability and maintainability

--- index.js
+++ index.js
@@ -3,34 +3,48 @@ const cards = document.querySelectorAll('.card');
 var clicked = 0;
 var firstClick = null;
 var secondClick = null;
+var firstCard = null;
+var secondCard = null;
+
+var lockBoard = false;
 
 function resetCards() {
     setTimeout(() => {
         clicked = 0;
-        cards.forEach(card => {
-            card.textContent = '?';
-        });
+        firstCard.textContent = '?';
+        secondCard.textContent = '?';
+        firstClick = null;
+        secondClick = null;
+        firstCard = null;
+        secondCard = null;
+        lockBoard = false;
     }, 1000);
 
 };
 
 cards.forEach(card => {
     card.addEventListener('click', () => {
-        if (clicked < 2) {
-            const value = card.getAttribute('data-card');
-            console.log('Card clicked, value:', value)
-            card.textContent = value;
-            clicked += 1;
-            if (clicked == 1)
-                firstClick = value;
-            else {
-                secondClick = value
-                if (firstClick == secondClick) {
-                    console.log("+1 score");
-                    clicked = 0;
-                } else
-                    resetCards();
-            };
+        if (card.textContent !== '?' || lockBoard) return;
+        const value = card.getAttribute('data-card');
+        console.log('Card clicked, value:', value)
+        card.textContent = value;
+        clicked += 1;
+        if (clicked == 1) {
+            firstCard = card;
+            firstClick = value;
+        } else {
+            secondCard = card;
+            secondClick = value
+            lockBoard = true;
+            if (firstClick == secondClick) {
+                console.log("+1 score");
+                clicked = 0;
+                firstClick = null;
+                secondClick = null;
+                firstCard = null;
+                secondCard = null;
+            } else
+                resetCards();
         };
 
     });
