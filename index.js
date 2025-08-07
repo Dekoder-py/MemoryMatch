3dd51bdadbd38be76ec0780772f10e8efe8e292e
Dekoder-py <dekod3r.py@gmail.com>
2025-08-07 17:31:50 +1200

    add randomisation and create all buttons in js

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-const cards = document.querySelectorAll('.card');
-
 var clicked = 0;
 var firstClick = null;
 var secondClick = null;
@@ -37,11 +35,41 @@ function checkWin() {
     }
 };
 
+const values = [];
+const numOfPairs = 6;
+
+
+for (i = 1; i <= numOfPairs; i++) {
+    values.push(i);
+    values.push(i);
+};
+
+// Fisher–Yates Shuffle
+function shuffle(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+};
+
+const board = document.querySelector('.cards');
+
+shuffle(values);
+for (var i = 0; i < values.length; i++) {
+    const card = document.createElement('button');
+    card.className = 'card';
+    card.setAttribute('data-card', values[i]);
+    card.textContent = '?';
+    board.appendChild(card);
+};
+
+const cards = document.querySelectorAll('.card');
+
 cards.forEach(card => {
     card.addEventListener('click', () => {
         if (card.textContent !== '?' || lockBoard) return;
         const value = card.getAttribute('data-card');
-        console.log('Card clicked, value:', value)
         card.textContent = value;
         clicked += 1;
         if (clicked == 1) {
